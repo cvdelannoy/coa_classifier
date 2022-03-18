c4158f8123d45f8c6dea9c885c0cd544daf83eee
noord087 <ben.noordijk@wur.nl>
2022-03-18 13:36:31 +0100

    Added list of coas that is parsed

--- run_production_pipeline.py
+++ run_production_pipeline.py
@@ -11,16 +11,15 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file
 
 
 def main(args):
-
     db_dir = parse_output_path(f'{args.out_dir}dbs/')
     nn_dir = parse_output_path(f'{args.out_dir}nns/')
     logs_dir = parse_output_path(f'{args.out_dir}logs/')
-    if type(args.kmer_list) == str:
-        with open(args.kmer_list, 'r') as fh: kmer_list = [k.strip() for k in fh.readlines() if len(k.strip())]
-    elif type(args.kmer_list) == list:
-        kmer_list = args.kmer_list
+    if type(args.coa_list) == str:
+        with open(args.coa_list, 'r') as fh: coa_list = [k.strip() for k in fh.readlines() if len(k.strip())]
+    elif type(args.coa_list) == list:
+        coa_list = args.coa_list
     else:
-        raise ValueError(f'dtype of kmer_list not valid: {type(args.kmer_list)}')
+        raise ValueError(f'dtype of coa_list not valid: {type(args.coa_list)}')
     with open(args.parameter_file, 'r') as pf: params = yaml.load(pf, Loader=yaml.FullLoader)
 
     # Construct and run snakemake pipeline
@@ -31,14 +30,10 @@ def main(args):
         nn_dir=nn_dir,
         logs_dir=logs_dir,
         parameter_file=args.parameter_file,
-        train_reads=args.training_reads,
-        test_reads=args.test_reads,
-        read_index=args.read_index,
-        read_index_bool=[True, False][args.read_index is None],
-        kmer_list=kmer_list,
+        train_reads=args.training_abfs,
+        test_reads=args.test_abfs,
+        kmer_list=coa_list,
         filter_width=params['filter_width'],
-        hdf_path=args.hdf_path,
-        uncenter_kmer=args.uncenter_kmer
     )
 
     sf_fn = f'{args.out_dir}nn_production_pipeline.sf'
