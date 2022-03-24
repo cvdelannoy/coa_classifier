5af219fb070bdc4a09b42a89d33e899ddfb3a3f9
noord087 <ben.noordijk@wur.nl>
2022-03-24 10:35:37 +0100

    Added option for oversampling to DB

--- db_building/CoaExampleDb.py
+++ db_building/CoaExampleDb.py
@@ -1,14 +1,17 @@
-import ZODB, ZODB.FileStorage, BTrees.IOBTree
+import ZODB, ZODB.FileStorage
+from persistent.mapping import PersistentMapping
 from os.path import isfile
 import numpy as np
 import random
-from db_building.AbfData import AbfData
-from copy import deepcopy
+from db_building.AbfData import AbfData, coa_to_one_hot
 
 
 class ExampleDb(object):
     """
-    A class for a database storing training examples for a neural network
+    Database storing training examples for a neural network
+    :param db_name: path to database file
+    :param width: width of fragments
+    :param read_only (optional):
     """
     def __init__(self, **kwargs):
         self._db = None
@@ -24,50 +27,71 @@ class ExampleDb(object):
     def add_training_read(self, training_read):
         """Add training read with cOA events
 
-        :param training_read: Training read
+        :param training_read: Object containing a training read
         :type training_read: AbfData
+        :return: Number of added events
         """
-        label = training_read.get_one_hot()
+        label = training_read.coa_type
 
-        # --- add positive examples ---
         with self._db.transaction() as conn:
+            if label not in conn.root.examples:
+                conn.root.examples[label] = []
+
+            # --- add positive examples (if any) ---
             pos_examples = training_read.get_pos(self.width)
             for ex in pos_examples:
-                insert_index = len(conn.root.pos)
-                conn.root.pos[insert_index] = ex
-                conn.root.labels[insert_index] = label
-            self.nb_pos = conn.root.pos.maxKey() if conn.root.pos else 0
+                conn.root.examples[label].append(ex)
+            nb_new_positives = len(pos_examples)
 
-        nb_examples = len(pos_examples)
-        print(f'Added {nb_examples} examples for {training_read.coa_type}')
-        if self._db_empty:
-            if nb_examples > 0:
+            # --- update record nb positive examples ---
+            if self._db_empty and nb_new_positives > 0:
                 self._db_empty = False
+                self.nb_pos = nb_new_positives
+            else:
+                self.nb_pos += nb_new_positives
+        print(f'Added {nb_new_positives} examples for {training_read.coa_type}')
 
-        return nb_examples
+        return nb_new_positives
 
-    def get_training_set(self, size=None):
+    def get_training_set(self, size=None, oversampling=False):
         """
         Return a balanced subset of reads from the DB
         :param size: number of reads to return
         :return: lists of numpy arrays for training data(x_out) and labels (y_out)
         """
-        # TODO is this actually balanced?
         if size is None or size > self.nb_pos:
             size = self.nb_pos
-        ps = random.sample(range(self.nb_pos), size)
+
+        training_set_x = []
+        training_set_y = []
 
         with self._db.transaction() as conn:
-            examples_pos = [(conn.root.pos[n], conn.root.labels[n]) for n in ps]
+            total_coas = len(conn.root.examples.keys())
+            # Get which class has least examples.
+            # Get how many items are expected per key based on set size
+            items_per_key = size / total_coas
+            if oversampling:
+                nr_of_examples = int(items_per_key)
+            else:
+                least_examples_in_class = min([len(v) for v in conn.root.examples.values()])
+                nr_of_examples = int(min(items_per_key, least_examples_in_class))
 
-        random.shuffle(examples_pos)
-        x_out, y_out = zip(*examples_pos)
-        return x_out, np.array(y_out)
+            for coa, signals in conn.root.examples.items():
+                one_hot = coa_to_one_hot(coa)
+                training_set_x.extend(random.choices(signals, k=nr_of_examples))
+                training_set_y.extend([one_hot] * nr_of_examples)
+
+        # Shuffle
+        c = list(zip(training_set_x, training_set_y))
+        random.shuffle(c)
+        training_set_x, training_set_y = zip(*c)
+
+        return training_set_x, training_set_y
 
     def pack_db(self):
         self._db.pack()
         with self._db.transaction() as conn:
-            self.nb_pos = conn.root.pos.maxKey() if conn.root.pos else 0
+            self.nb_pos = sum([len(i) for i in conn.root.examples.values()])
 
     @property
     def db(self):
@@ -85,11 +109,11 @@ class ExampleDb(object):
         if is_existing_db:
             with self._db.transaction() as conn:
                 self.width = conn.root.width
-                self.nb_pos = len(conn.root.pos)
+                # Get total number of entries in database
+                self.nb_pos = sum([len(i) for i in conn.root.examples.values()])
         else:
             with self._db.transaction() as conn:
                 conn.root.width = self.width
-                conn.root.pos = BTrees.IOBTree.BTree()
-                conn.root.labels = BTrees.IOBTree.BTree()
+                conn.root.examples = PersistentMapping()
         if self.nb_pos > 0:
             self._db_empty = False
