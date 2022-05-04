06133e30175ef43d94a37528d36b9dc5a77c7ce6
noord087 <ben.noordijk@wur.nl>
2022-05-04 17:08:15 +0200

    Updated inference to work for all 4 coas and new model architecture

--- run_inference.py
+++ run_inference.py
@@ -1,16 +1,21 @@
 from pathlib import Path
 import tensorflow as tf
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
 from db_building.AbfData import AbfData
 from collections import Counter
 from sklearn.metrics import confusion_matrix, balanced_accuracy_score
 
 # Map target coa to correct index
-TARGET_TO_INDEX = {'cOA4': 0,
-                   'cOA5': 1,
-                   'cOA6': 2}
+TARGET_TO_INDEX = {'cOA3': 0,
+                   'cOA4': 1,
+                   'cOA5': 2,
+                   'cOA6': 3}
 
 INDEX_TO_TARGET = {v: k for k, v in TARGET_TO_INDEX.items()}
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+tf.config.set_visible_devices(gpus[1:], 'GPU')
 
 class CoaInference:
     """Class that uses generated CNNs to detect cOAs in abf files
@@ -18,22 +23,9 @@ class CoaInference:
     :param nn_dir: Path to directory that contains folders with cOA?/nn.h5 files
     :type nn_dir: Path
     """
-    def __init__(self, nn_dir):
-        self._nn_dir = nn_dir
-        self.model_dict, self.input_width = self._load_models()
-
-    def _load_models(self):
-        """Called during __init__ and loads all CNNs into a model dictionary"""
-        model_dict = {}
-        all_model_files = list(self._nn_dir.glob('cOA?/nn.h5'))
-        assert all_model_files, 'No generated neural networks were found'
-        for f in all_model_files:
-            # Extract target COA from folder name
-            target = f.parts[-2]
-            model = tf.keras.models.load_model(f, compile=False)
-            model_dict[target] = model
-        input_width = model.input_shape[1]
-        return model_dict, input_width
+    def __init__(self, nn):
+        self.nn = tf.keras.models.load_model(nn)
+        self.input_length = self.nn.input_shape[1]
 
     def predict_from_file(self, abf_path):
         """Counts cOAs in single input file
@@ -44,37 +36,32 @@ class CoaInference:
         :rtype: Counter
         """
         abf = AbfData(abf_path)
-        events = abf.get_pos()
-        x_pad = np.expand_dims(events, -1)
-
-        # ndarray, first axis is event no, second axis per-coa confidence score
-        prediction = np.zeros((len(events), len(self.model_dict)))
-        for target, model in self.model_dict.items():
-            index = TARGET_TO_INDEX[target]
-            prediction[:, index] = model.predict(x_pad).flatten()
-
-        # Find which of the three models predicted the highest score
-        highest_index = np.argmax(prediction, axis=1)
-        labels = [INDEX_TO_TARGET[i] for i in highest_index]
-        return Counter(labels)
+        events = abf.get_pos(unfiltered=True)
+        x_pad = np.expand_dims(pad_sequences(events, maxlen=self.input_length,
+                                             padding='post', truncating='post',
+                                             dtype='float32'), -1)
+        y_hat = self.nn.predict(x_pad)
+        y_hat = np.argmax(y_hat, axis=1)
+        return [INDEX_TO_TARGET[i] for i in y_hat]
 
 def main(args):
-    inference_model = CoaInference(args.nn_dir)
+    inference_model = CoaInference(args.nn_path)
 
     y_true = []
     y_pred_list = []
 
-    for abf in Path(args.abf_in).iterdir():
+    for i, abf in enumerate(Path(args.abf_in).iterdir()):
         # # Uncomment to omit the coa5 file
         # if 'cOA5' in abf.name:
         #     continue
         print(f'Processing {abf.name}')
         y_pred = inference_model.predict_from_file(str(abf))
-        print(y_pred)
+        # print(y_pred)
         true_coa = abf.name[:4]
-        y_true.extend([true_coa] * sum(y_pred.values()))
-        for coa_pred, count in y_pred.items():
-            y_pred_list.extend([coa_pred] * count)
+        y_true.extend([true_coa] * len(y_pred))
+        y_pred_list.extend(y_pred)
+        if i > 5:
+            break
 
     print(confusion_matrix(y_true, y_pred_list))
     print('Balanced accuracy', balanced_accuracy_score(y_true, y_pred_list))
